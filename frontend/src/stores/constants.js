1d6aa0f50b85424a584201aad0df410402970d63
Evelyn <53372490+CodyPenny@users.noreply.github.com>
2022-02-26 01:30:52 -0800

    Create constants.js

---
+++ frontend/src/stores/constants.js
@@ -0,0 +1,46 @@
+export const HOME = [
+  { key: 's', text: 'Single Family', value: 'Single Family' },
+  { key: 'c', text: 'Condo', value: 'Condo' },
+  { key: 't', text: 'Townhouse', value: 'Townhouse' },
+];
+
+export const BED = [
+  { key: '0', text: '0', value: 0 },
+  { key: '1', text: '1', value: 1 },
+  { key: '2', text: '2', value: 2 },
+  { key: '3', text: '3', value: 3 },
+  { key: '4', text: '4', value: 4 },
+  { key: '5', text: '5', value: 5 },
+  { key: '6', text: '6', value: 6 },
+  { key: '7', text: '7', value: 7 },
+  { key: '8', text: '8', value: 8 },
+];
+
+export const BATH = [
+  { key: '1', text: '1', value: 1 },
+  { key: '2', text: '1.5', value: 1.5 },
+  { key: '3', text: '2', value: 2 },
+  { key: '4', text: '2.5', value: 2.5 },
+  { key: '5', text: '3', value: 3 },
+  { key: '6', text: '3.5', value: 3.5 },
+  { key: '7', text: '4', value: 4 },
+];
+
+export const STORY = [
+  { key: '1', text: '1', value: 1 },
+  { key: '2', text: '2', value: 2 },
+  { key: '3', text: '3', value: 3 },
+];
+
+// Remove BASEURL at deployment
+export const BASEURL = 'http://localhost:5000'
+
+export const PREDICT = '/api/predict'
+
+export const LOCATION = '/api/latlong'
+
+export const GRAPHS ='/api/graph/yearbuilt'
+
+export const YEAR = 2021;
+
+export const MIN_YEAR = 1950;
