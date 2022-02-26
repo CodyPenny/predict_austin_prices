7a4b5939b7b6afc4e0c6369f11cb8f2d0da9d22d
Evelyn <53372490+CodyPenny@users.noreply.github.com>
2022-02-26 01:33:38 -0800

    Create StyledHeader.js

---
+++ frontend/src/styled/StyledHeader.js
@@ -0,0 +1,88 @@
+import styled from 'styled-components';
+
+/**
+ * Styled components to handle different media screens
+ */
+export const StyledHeaderMobile_A = styled.div({
+    fontSize: '2.5em',
+    fontWeight: 'normal',
+    paddingTop: 'calc(2em + 1vh)',
+    marginLeft: 'auto', 
+    marginRight: 'auto',
+    marginBottom: 0,
+    width: '75vw',
+    textAlign: 'center',
+    background: '#1b1c1d'
+})
+export const StyledHeaderMobile_B = styled.h2`{
+    font-size: 1.2em;
+    font-weight: normal;
+    padding-left: 3em;
+    padding-right: 3em;
+    padding-top: calc(2em + 1vh);
+    line-height: 1.6em;
+    color: #fff;
+    background: #1b1c1d;
+    width: 75vw;
+    margin: 0 auto;
+    text-align: center;
+}`
+
+export const StyledHeaderTablet_A = styled.div({
+    fontSize: '3em',
+    fontWeight: 'normal',
+    paddingTop: 'calc(2em + 1vh)',
+    marginLeft: 'auto', 
+    marginRight: 'auto',
+    marginBottom: 0,
+    width: '75vw',
+    textAlign: 'center',
+    background: '#1b1c1d',
+    color: '#fff'
+})
+export const StyledHeaderTablet_B = styled.h2`{
+    font-size: 1.5em;
+    font-weight: normal;
+    padding-left: 4em;
+    padding-right: 4em;
+    padding-top: calc(2em + 1vh);
+    line-height: 1.6em;
+    color: #fff;
+    background: #1b1c1d;
+    width: 75vw;
+    margin: 0 auto;
+    text-align: center;
+}`
+
+export const StyledHeaderDeskTop_A = styled.div({
+    fontSize: '4em',
+    fontWeight: 'normal',
+    paddingTop: 'calc(2em + 1vh) !important',
+    marginLeft: 'auto', 
+    marginRight: 'auto',
+    marginBottom: 0,
+    width: '75vw',
+    textAlign: 'center',
+    background: '#1b1c1d !important',
+    color: '#fff'
+})
+export const StyledHeaderDeskTop_B = styled.h2`{
+    font-size: 1.8em;
+    font-weight: normal;
+    padding-left: 6em;
+    padding-right: 6em;
+    padding-top: calc(2em + 1vh);
+    line-height: 1.6em;
+    color: #fff;
+    background: #1b1c1d;
+    width: 75vw;
+    margin: 0 auto;
+    text-align: center;
+}`
+
+export const StyledButtonWrapper = styled.div` {
+    background: #1b1c1d;
+    padding-top: 2em;
+    padding-bottom: 4em;
+}
+`
