0acc76227bd6ca753b27db49d08eabd717a95f17
Evelyn <53372490+CodyPenny@users.noreply.github.com>
2022-02-26 01:34:05 -0800

    Create StyledSegment.js

---
+++ frontend/src/styled/StyledSegment.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { Segment } from 'semantic-ui-react';
+
+export const StyledSegmentWrapper= styled(Segment)({
+    width: '75vw', 
+    marginLeft: 'auto !important', 
+    marginRight: 'auto !important',
+    background: '#1b1c1d !important',
+    marginTop: '0 !important',
+})
+
+export const StyledSegmentTablet = styled(Segment)({
+    padding: '1.5em 0em',
+    width: '75vw', 
+    marginLeft: 'auto', 
+    marginRight: 'auto',
+})
+
+export const StyledSegmentDeskTop = styled(Segment)({
+    padding: '1.5em 0em',
+    width: '75vw', 
+    marginLeft: 'auto', 
+    marginRight: 'auto'
+})
+
